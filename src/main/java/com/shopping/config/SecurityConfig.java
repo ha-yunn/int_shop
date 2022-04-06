c543bd4291af0a121d03d13381e8d3bbb2705b41
ha-yunn <96652845+ha-yunn@users.noreply.github.com>
2022-04-06 18:05:36 +0900

    4. Sign up

--- src/main/java/com/shopping/config/SecurityConfig.java
+++ src/main/java/com/shopping/config/SecurityConfig.java
@@ -14,8 +14,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
